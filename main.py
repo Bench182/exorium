e61630eeada79b372028b15d1bbc8c176124ca0e
Chosen-Fate <55001472+Chosen-Fate@users.noreply.github.com>
2020-06-06 18:39:50 +0200

    Added poll to help, info

--- main.py
+++ main.py
@@ -55,8 +55,8 @@ async def latency(ctx):
 @bot.command(name='help')
 async def help(ctx):
     embed = discord.Embed(title='Help menu - Prefixes `p!` | `?`', color=config.color)
-    embed.add_field(name="**commands**", value="\n`help`\n`ping`\n`invite`\n`stats`\n`get_id`\n`av`\n`links`\n`snuggle`\n`hug`\n`pat`\n`boop`\n`kiss`\n`random`\n`info`\n`honk`\n`askreggie`\n`ban`\n`unban\nkick\nsoftban`", inline=True)
-    embed.add_field(name="**Description**", value="`shows help menu\nshows bot latency\nbot invite link\nglobal bot stats\nget user ID\nget user avatar\nrelated links\nsnuggle someone\nhug someone\npat someone\nboop someone\nsmooch someone\nrandom selection\nshows command info\nHONKS\nAsk Reggie a question\nban a member\nunban a member\nkick a member\nsoftban a member`", inline=True)
+    embed.add_field(name="**commands**", value="\n`help`\n`ping`\n`invite`\n`stats`\n`get_id`\n`av`\n`links`\n`snuggle`\n`hug`\n`pat`\n`boop`\n`kiss`\n`random`\n`info`\n`honk`\n`askreggie`\n`ban`\n`unban\nkick\nsoftban\npoll`", inline=True)
+    embed.add_field(name="**Description**", value="`shows help menu\nshows bot latency\nbot invite link\nglobal bot stats\nget user ID\nget user avatar\nrelated links\nsnuggle someone\nhug someone\npat someone\nboop someone\nsmooch someone\nrandom selection\nshows command info\nHONKS\nask Reggie a question\nban a member\nunban a member\nkick a member\nsoftban a member\ncast a poll`", inline=True)
     embed.add_field(name="developers:", value="`-` ChosenFate#5108\n`-` Bluewydahoosk#2923", inline=False)
     embed.set_thumbnail(url="https://www.dropbox.com/s/yx7z6iefnx0q576/Icon.jpg?dl=1")
     embed.set_footer(text="Thank you, " + ctx.message.author.name + ", for being a part of The Paw Kingdom!")
@@ -349,6 +349,7 @@ class cmds:
     ban = "Bans the mentioned person"
     kick = "Kicks the specified person"
     softban = "Softbans (bans and unbans) the specified"
+    poll = "Cast a poll if you can't agree about something!"
 
 
 class syntax:
@@ -372,6 +373,7 @@ class syntax:
     ban = "`?ban @user | ID Reason`"
     kick = "`?kick @user | ID reason`"
     softban = "`?softban @user | ID reason"
+    poll = "`?poll choice1, choice2, choice3 [...]`"
 
 
 print(discord.__version__)
