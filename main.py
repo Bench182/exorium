dd5da6cc4bd034dfcd4268280c01e1528fc5a4cf
Bluewy <bluewyechache@gmail.com>
2020-07-29 19:04:13 +0200

    Added more autoresponses

--- main.py
+++ main.py
@@ -349,6 +349,13 @@ async def on_message(message):
     
     if "cutie" in message.content.lower():
         await message.channel.send("no u")
+        
+    
+    if "c u t e" in message.content.lower():
+        await message.channel.send("no u")
+    
+    if "c ute" in message.content.lower():
+        await message.channel.send("no u")
     await bot.process_commands(message)
 
 class cmds:
