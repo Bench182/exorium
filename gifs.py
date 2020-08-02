7c37625442493c68e87ce447fe76f6075e5edb3f
Bluewy <bluewyechache@gmail.com>
2020-08-02 12:47:17 +0200

    Changed how gif lists are displayed

--- gifs.py
+++ gifs.py
@@ -1,20 +1,73 @@
-BoopList = ["https://media1.tenor.com/images/36d187af3d5e876ec0d8f11cabe1a8d4/tenor.gif?itemid=15225693", "https://media1.tenor.com/images/ea37d5c281b7ca7c35175bdc854b0a5b/tenor.gif?itemid=11159084", "https://media.tenor.com/images/3f364e971453b18a1ae2b5cc8d2d0613/tenor.gif", "https://media1.tenor.com/images/a0f0c6b3ef95bb2ff530a92d6c516cbd/tenor.gif?itemid=14452125", "https://media1.tenor.com/images/5c0b2e9661b5ea40834fc6bd6b84a806/tenor.gif?itemid=14796547", "https://cdn.discordapp.com/attachments/715979499799445577/717050110667784272/8NgFDIL.gif", "https://media.tenor.com/images/aa41ce7ea8f138eb8e0be635cd4664d9/tenor.gif", "https://media1.tenor.com/images/9e42f5059137a153137af77cb898c070/tenor.gif?itemid=17599907"]
+BoopList = ["https://media1.tenor.com/images/36d187af3d5e876ec0d8f11cabe1a8d4/tenor.gif?itemid=15225693", 
+            "https://media1.tenor.com/images/ea37d5c281b7ca7c35175bdc854b0a5b/tenor.gif?itemid=11159084", 
+            "https://media.tenor.com/images/3f364e971453b18a1ae2b5cc8d2d0613/tenor.gif", 
+            "https://media1.tenor.com/images/a0f0c6b3ef95bb2ff530a92d6c516cbd/tenor.gif?itemid=14452125", 
+            "https://media1.tenor.com/images/5c0b2e9661b5ea40834fc6bd6b84a806/tenor.gif?itemid=14796547", 
+            "https://cdn.discordapp.com/attachments/715979499799445577/717050110667784272/8NgFDIL.gif", 
+            "https://media.tenor.com/images/aa41ce7ea8f138eb8e0be635cd4664d9/tenor.gif", 
+            "https://media1.tenor.com/images/9e42f5059137a153137af77cb898c070/tenor.gif?itemid=17599907"]
 
-PatList = ["https://media1.tenor.com/images/1786de6797fd096ff503d633254786cc/tenor.gif?itemid=14598566", "https://media1.tenor.com/images/be0c22e0af951aa7fa8753381663eb2c/tenor.gif?itemid=15824856", "https://media1.tenor.com/images/fd7365b518ee53285f39ef8b1caf5b6e/tenor.gif?itemid=15523978", "https://media1.tenor.com/images/adeb267fbe4ab5ddaa080297e5b2d0e4/tenor.gif?itemid=15740749", "https://media1.tenor.com/images/62d861f1f0b6277e2056e86f4ba42e6f/tenor.gif?itemid=16276220"]
+PatList = ["https://media1.tenor.com/images/1786de6797fd096ff503d633254786cc/tenor.gif?itemid=14598566", 
+           "https://media1.tenor.com/images/be0c22e0af951aa7fa8753381663eb2c/tenor.gif?itemid=15824856", 
+           "https://media1.tenor.com/images/fd7365b518ee53285f39ef8b1caf5b6e/tenor.gif?itemid=15523978", 
+           "https://media1.tenor.com/images/adeb267fbe4ab5ddaa080297e5b2d0e4/tenor.gif?itemid=15740749", 
+           "https://media1.tenor.com/images/62d861f1f0b6277e2056e86f4ba42e6f/tenor.gif?itemid=16276220"]
 
-HugList = ["https://media1.tenor.com/images/faf29ddc23059c5740751a3fecc2f303/tenor.gif?itemid=13533690", "https://media1.tenor.com/images/f20151a1f7e003426ca7f406b6f76c82/tenor.gif?itemid=13985247", "https://media1.tenor.com/images/8ac5ada8524d767b77d3d54239773e48/tenor.gif?itemid=16334628", "https://media1.tenor.com/images/68f16d787c2dfbf23a4783d4d048c78f/tenor.gif?itemid=9512793", "https://media1.tenor.com/images/3fee00811a33590e4ee490942f233c78/tenor.gif?itemid=14712845", "https://media1.tenor.com/images/2d13ede25b31d946284eaa3b8a4e6b31/tenor.gif?itemid=11990658", "https://media1.tenor.com/images/37df0fae36f9cce061c3cec84fc97a08/tenor.gif?itemid=17781844", "https://media1.tenor.com/images/ece75ca15b715aacd86724ee23604569/tenor.gif?itemid=16796068", "https://media2.giphy.com/media/gl8ymnpv4Sqha/giphy.gif"]
+HugList = ["https://media1.tenor.com/images/faf29ddc23059c5740751a3fecc2f303/tenor.gif?itemid=13533690", 
+           "https://media1.tenor.com/images/f20151a1f7e003426ca7f406b6f76c82/tenor.gif?itemid=13985247", 
+           "https://media1.tenor.com/images/8ac5ada8524d767b77d3d54239773e48/tenor.gif?itemid=16334628", 
+           "https://media1.tenor.com/images/68f16d787c2dfbf23a4783d4d048c78f/tenor.gif?itemid=9512793", 
+           "https://media1.tenor.com/images/3fee00811a33590e4ee490942f233c78/tenor.gif?itemid=14712845", 
+           "https://media1.tenor.com/images/2d13ede25b31d946284eaa3b8a4e6b31/tenor.gif?itemid=11990658", 
+           "https://media1.tenor.com/images/37df0fae36f9cce061c3cec84fc97a08/tenor.gif?itemid=17781844", 
+           "https://media1.tenor.com/images/ece75ca15b715aacd86724ee23604569/tenor.gif?itemid=16796068", 
+           "https://media2.giphy.com/media/gl8ymnpv4Sqha/giphy.gif"]
 
-SnuggleList = ["https://media1.tenor.com/images/ba7829e33a9392d38e15cdc4c650b897/tenor.gif?itemid=17325139", "https://media.giphy.com/media/kz01wlAoCLYx3v8F68/giphy.gif", "https://media.giphy.com/media/DAX8RixOTrWuY/giphy.gif"]
+SnuggleList = ["https://media1.tenor.com/images/ba7829e33a9392d38e15cdc4c650b897/tenor.gif?itemid=17325139", 
+               "https://media.giphy.com/media/kz01wlAoCLYx3v8F68/giphy.gif", 
+               "https://media.giphy.com/media/DAX8RixOTrWuY/giphy.gif"]
 
-KissList = ["https://media1.tenor.com/images/473c30ffa595a67a281d9b2fde7b48d6/tenor.gif?itemid=5373130", "https://media.tenor.com/images/78aaf08e4cb6174a5d3a0d2826c43953/tenor.gif", "https://media1.tenor.com/images/3fce8ce3fb358cc6fafd0517c6558653/tenor.gif?itemid=14802231", "https://cdn.weasyl.com/~recurrent/submissions/1797089/3e4439c6a3b09383c2361d2db9f5d72f1e74f70dfaf26654b468875ee1d0071d/recurrent-c-kiss-under-the-moon.gif", "https://pa1.narvii.com/6929/e06bf443286d81e79374a4374cac98a1e263ebb4r1-2000-2000_hq.gif", "https://i.kym-cdn.com/photos/images/original/001/099/490/e82.gif", "https://media1.tenor.com/images/12ff73f58a57174d6c6228f323e41c21/tenor.gif?itemid=11770367", "https://d.facdn.net/art/kusuguttai/1550707158/1550707158.kusuguttai_kissandnibble.gif", "https://i.imgur.com/ApV7HF5.gif", "https://i.pinimg.com/originals/a2/64/72/a26472dd131d20dba037b81c1a633c61.gif", "https://media1.tenor.com/images/5c8972db3df932240bb191be36c71b85/tenor.gif?itemid=7189564"]
+KissList = ["https://media1.tenor.com/images/473c30ffa595a67a281d9b2fde7b48d6/tenor.gif?itemid=5373130", 
+            "https://media.tenor.com/images/78aaf08e4cb6174a5d3a0d2826c43953/tenor.gif", 
+            "https://media1.tenor.com/images/3fce8ce3fb358cc6fafd0517c6558653/tenor.gif?itemid=14802231", 
+            "https://cdn.weasyl.com/~recurrent/submissions/1797089/3e4439c6a3b09383c2361d2db9f5d72f1e74f70dfaf26654b468875ee1d0071d/recurrent-c-kiss-under-the-moon.gif", 
+            "https://pa1.narvii.com/6929/e06bf443286d81e79374a4374cac98a1e263ebb4r1-2000-2000_hq.gif", 
+            "https://i.kym-cdn.com/photos/images/original/001/099/490/e82.gif", 
+            "https://media1.tenor.com/images/12ff73f58a57174d6c6228f323e41c21/tenor.gif?itemid=11770367", 
+            "https://d.facdn.net/art/kusuguttai/1550707158/1550707158.kusuguttai_kissandnibble.gif", 
+            "https://i.imgur.com/ApV7HF5.gif", 
+            "https://i.pinimg.com/originals/a2/64/72/a26472dd131d20dba037b81c1a633c61.gif", 
+            "https://media1.tenor.com/images/5c8972db3df932240bb191be36c71b85/tenor.gif?itemid=7189564"]
 
-LickList = ["https://i.kym-cdn.com/photos/images/original/001/099/490/e82.gif", "https://media1.tenor.com/images/1b96b02b98756a1a45fdde87dacbb36b/tenor.gif?itemid=17522184", "https://media.giphy.com/media/8v9njaWnlcW2c/giphy.gif"]
+LickList = ["https://i.kym-cdn.com/photos/images/original/001/099/490/e82.gif", 
+            "https://media1.tenor.com/images/1b96b02b98756a1a45fdde87dacbb36b/tenor.gif?itemid=17522184", 
+            "https://media.giphy.com/media/8v9njaWnlcW2c/giphy.gif"]
 
-AskProtopaw = ["yes", "no", "maybe", "probably", "certainly", "unlikely", "absolutely", "the answer to that question may be beyond the knowledge of any of us, even Proto...", "What?? Proto is a Bot, not Einstein...", "of course", "you know already ;)", "don't ask Proto that!", "never.", "that is an Area 51 level secret; Proto knows, but Proto can't tell...", "okay", "sure", "that this is an example of a stupid question"]
+AskProtopaw = ["yes", 
+               "no", 
+               "maybe", 
+               "probably", 
+               "certainly", 
+               "unlikely", 
+               "absolutely", 
+               "the answer to that question may be beyond the knowledge of any of us, even Proto...", 
+               "What?? Proto is a Bot, not Einstein...", 
+               "of course", 
+               "you know already ;)", 
+               "don't ask Proto that!", 
+               "never.", 
+               "that is an Area 51 level secret; Proto knows, but Proto can't tell...", 
+               "okay", 
+               "sure", 
+               "that this is an example of a stupid question"]
 
-CuddleList = ["https://thumbs.gfycat.com/WaterloggedFabulousIbis-small.gif", "https://media3.giphy.com/media/h4BprYiFYNxRe/giphy.gif"]
+CuddleList = ["https://thumbs.gfycat.com/WaterloggedFabulousIbis-small.gif", 
+              "https://media3.giphy.com/media/h4BprYiFYNxRe/giphy.gif"]
 
-BellyrubList = ["https://i.imgur.com/VnoVdts.gif", "https://i.imgur.com/AGb1AKb.gif", "https://i.imgur.com/PbYykOz.gif", "https://i.imgur.com/xxx0Ykb.gif"]
+BellyrubList = ["https://i.imgur.com/VnoVdts.gif", 
+                "https://i.imgur.com/AGb1AKb.gif", 
+                "https://i.imgur.com/PbYykOz.gif", 
+                "https://i.imgur.com/xxx0Ykb.gif"]
 
 AwooList = ["https://cdn.discordapp.com/attachments/715969701771083820/739429902402715748/unknown.gif", 
             "https://cdn.discordapp.com/attachments/726377118304305245/739430343962001468/unknown.gif",
