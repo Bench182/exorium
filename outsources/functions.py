ef71a8476953024aa9f76b8a6cc1f78038362069
Chosen-Fate <55001472+Chosen-Fate@users.noreply.github.com>
2020-08-03 00:59:17 +0200

    Make a function for all the interacion commands, cut down code

---
+++ outsources/functions.py
@@ -0,0 +1,14 @@
+import discord
+import config
+import random
+import gifs
+
+
+async def interactions(ctx, members, reason, type, ending, typespecial):
+    GIFlist = getattr(gifs, type)
+    GIF = random.choice(GIFlist)
+    if not (members):
+        return await ctx.send(f"Please specify at least one cutie to {type}!")
+    embed = discord.Embed(title="", color=config.color, description=(ctx.message.author.mention + " " + f"**{typespecial}**" + " " + '**,** '.join(x.mention for x in members) + f"**, {ending}!**\nFor: " + reason))
+    embed.set_image(url=GIF)
+    await ctx.send(embed=embed)
