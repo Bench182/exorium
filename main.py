e73e53ef7ec0e979d0ff698f626ce395094e26a4
Bluewy <bluewyechache@gmail.com>
2020-08-10 14:39:45 +0200

    Edited dev name in info command

--- main.py
+++ main.py
@@ -200,7 +200,7 @@ async def info(ctx, arg):
     embed = discord.Embed(title='Help menu - Prefixes `p!` | `?`', color=config.color)
     embed.add_field(name=arg, value=getattr(cmds, arg), inline=True)
     embed.add_field(name="Syntax of " + arg, value=getattr(syntax, arg), inline=True)
-    embed.add_field(name="Developers:", value="`-` ChosenFate#5108\n`-` BluewytheRenegade#2923", inline=False)
+    embed.add_field(name="Developers:", value="`-` ChosenFate#5108\n`-` BluewyFurGame#5108", inline=False)
     embed.set_thumbnail(url="https://www.dropbox.com/s/yx7z6iefnx0q576/Icon.jpg?dl=1")
     embed.set_footer(text="Thank you, " + ctx.message.author.name + ", for using ProtoPaw!")
     await ctx.send(embed=embed)
