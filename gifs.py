ab291d1e5d114a298d58df9cab1213935fe7944a
Bluewy <bluewyechache@gmail.com>
2020-08-11 13:22:55 +0200

    Fixed cuddle command gifs

--- gifs.py
+++ gifs.py
@@ -66,12 +66,12 @@ AskProtopaw = ["yes",
 
 cuddle = ["https://thumbs.gfycat.com/WaterloggedFabulousIbis-small.gif",
           "https://media3.giphy.com/media/h4BprYiFYNxRe/giphy.gif",
-          "https://tenor.com/ZMZS.gif",
-          "https://tenor.com/xz93.gif",
-          "https://tenor.com/FL7y.gif",
-          "https://tenor.com/bfooz.gif",
-          "https://tenor.com/5Vbq.gif",
-          "https://tenor.com/bdXEX.gif"]
+          "https://media1.tenor.com/images/304d8bf0315c3096603afd7e6562833c/tenor.gif?itemid=12304006",
+          "https://media1.tenor.com/images/64ffd0c97258e49fd08f17397792c6c0/tenor.gif?itemid=5581481",
+          "https://media.tenor.com/images/5c02ca01064b6c2f75c6e1dbbbc5de90/tenor.gif",
+          "https://media1.tenor.com/images/e1035f7a2efb7722abd665f4e6a164be/tenor.gif?itemid=16022685",
+          "https://media1.tenor.com/images/2259bbd5696244422b64fc4c7d235a93/tenor.gif?itemid=13765442",
+          "https://media1.tenor.com/images/9d4d148f45d355cda83ada31b3112c85/tenor.gif?itemid=15681585"]
 
 bellyrub = ["https://i.imgur.com/VnoVdts.gif",
             "https://i.imgur.com/AGb1AKb.gif",
