ee512dfceaf6d696cea32b119676c38870aaaa96
Bluewy <bluewyechache@gmail.com>
2020-08-11 14:10:58 +0200

    Fixed indents

--- gifs.py
+++ gifs.py
@@ -101,6 +101,6 @@ blush = ["https://media1.tenor.com/images/68e8bca8e90cb37fdcc9cc798bbc13f5/tenor
          "https://media1.tenor.com/images/369329091222b088ddb788b6196be261/tenor.gif?itemid=17315979",
          "https://media1.tenor.com/images/969eafdd133b4a1d24cb9135b4bf6310/tenor.gif?itemid=14043564",
          "https://media1.tenor.com/images/41bc1ff3064c7a5aa0979c2ddee32dc8/tenor.gif?itemid=9869912",
-         "https://media1.tenor.com/images/e8ad00d3190adf65434672c075a4c9e1/tenor.gif?itemid=16242697",
-         ]
+         "https://media1.tenor.com/images/e8ad00d3190adf65434672c075a4c9e1/tenor.gif?itemid=16242697"]
+
 votechannels = [731848766327619586, 731829759146721320]
