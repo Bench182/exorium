46aafeeb0873abe04c9039b0a2e7c67b2b5a744a
Bluewy <bluewyechache@gmail.com>
2020-08-11 12:53:43 +0200

    Fixed indent issue

--- gifs.py
+++ gifs.py
@@ -91,7 +91,6 @@ rawr = ["https://media1.tenor.com/images/5393afcde44d4be4569108e40fddb3a1/tenor.
         "https://media1.tenor.com/images/0ffb715776443e6242b736420ce84144/tenor.gif?itemid=14558168",
         "https://media1.tenor.com/images/6f5c4dd655d8d93b5f85d2f739f11cea/tenor.gif?itemid=16986104",
         "https://media1.tenor.com/images/78e79fa52e3934f693ce670a3a905e64/tenor.gif?itemid=16549140"
-        "https://media1.tenor.com/images/84b80c7e1fa843fd4ea7b90e1bc269c4/tenor.gif?itemid=15426315",
-        ]
+        "https://media1.tenor.com/images/84b80c7e1fa843fd4ea7b90e1bc269c4/tenor.gif?itemid=15426315"]
 
 votechannels = [731848766327619586, 731829759146721320]
