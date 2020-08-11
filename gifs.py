7c783e1d6b819950ddc1df4e9b9e38e96449d8cf
Bluewy <bluewyechache@gmail.com>
2020-08-11 15:13:57 +0200

    Added more happy gifs

--- gifs.py
+++ gifs.py
@@ -118,5 +118,8 @@ happy = ["https://media1.tenor.com/images/4b5d8967536d7235f5231ba9123964ab/tenor
          "https://media.tenor.com/images/f7ba3433bb89a484ec92d33b87f54ca7/tenor.gif",
          "https://media.tenor.com/images/8784dfd64f06f19e1a25cb88f990b2b7/tenor.gif",
          "https://media1.tenor.com/images/aca1a8cd3d8d7bf7d5703bf3f8117a4b/tenor.gif?itemid=5567628",
-         "https://media1.tenor.com/images/ab69ea03faa81a44af51c86770745f61/tenor.gif?itemid=9273180"]
+         "https://media1.tenor.com/images/ab69ea03faa81a44af51c86770745f61/tenor.gif?itemid=9273180",
+         "https://media1.tenor.com/images/685f1055a8a4dbfe3318e5898d19b97c/tenor.gif?itemid=10253595",
+         "https://media1.tenor.com/images/41df672a1055e4f833e2022dce9231fd/tenor.gif?itemid=16660700",
+         "https://d.facdn.net/art/bluemachine/1573314972/1573314972.bluemachine_2.gif"]
 votechannels = [731848766327619586, 731829759146721320]
