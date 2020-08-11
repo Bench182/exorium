dece0eb77ad3d04c24d645fb6d52c03084d683f6
Bluewy <bluewyechache@gmail.com>
2020-08-11 13:20:45 +0200

    Fixed lick command gifs

--- gifs.py
+++ gifs.py
@@ -42,9 +42,9 @@ smooch = ["https://media1.tenor.com/images/473c30ffa595a67a281d9b2fde7b48d6/teno
 lick = ["https://i.kym-cdn.com/photos/images/original/001/099/490/e82.gif",
         "https://media1.tenor.com/images/1b96b02b98756a1a45fdde87dacbb36b/tenor.gif?itemid=17522184",
         "https://media.giphy.com/media/8v9njaWnlcW2c/giphy.gif",
-        "https://tenor.com/bedO1.gif",
-        "https://tenor.com/1Wra.gif",
-        "https://tenor.com/ZhcT.gif"]
+        "https://media1.tenor.com/images/f40e1b6271581699aed3c12d042c129c/tenor.gif?itemid=15743713",
+        "https://media1.tenor.com/images/cd4f8199d49badf7b90277f8b13f5186/tenor.gif?itemid=12816950",
+        "https://media1.tenor.com/images/cd45350c528cc577cbe0eb754e6e8241/tenor.gif?itemid=12181805"]
 
 AskProtopaw = ["yes",
                "no",
