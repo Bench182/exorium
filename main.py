1336badcfdbe4ca2b0aab994d7066fb2601c7aa0
Bluewy <bluewyechache@gmail.com>
2020-08-19 15:24:16 +0200

    Edited title and field value (contributors cmd)

--- main.py
+++ main.py
@@ -415,8 +415,8 @@ async def purge(ctx, amount=0):
   
 @bot.command()
 async def contributors(ctx):
-    embed = discord.Embed(title='Contributors', color=config.color)
-    embed.add_field(name='Contributions', value='Contributors and their commands are listed here.', inline=False)
+    embed = discord.Embed(title='command contributors', color=config.color)
+    embed.add_field(name='Contributions', value='Contributors and their commands are listed here.\nOnly actual contributed commands are listed here.', inline=False)
     embed.add_field(name='Commands', value='`purge`', inline=True)
     embed.add_field(name='Contributor', value='`Bench182#5276`', inline=True)
     embed.add_field(name='Other contributions', value="For all contributions please check [the repo](https://github.com/FireGamingYT/protogen)", inline=False)
