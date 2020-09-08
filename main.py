454b84ecd76c714eaaa9fc15cfac4b6a49759c72
Bluewy <bluewyechache@gmail.com>
2020-09-08 12:08:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ bot.remove_command('help')  # removes the default discord.py help command
 
 @bot.event  # sets the bot status and prints when it has started in console with stats, stats include: The amount of users that are in the total amount of guilds and the discord.py version
 async def on_ready():
-    activity = discord.Game(name=f'len{bot.guilds} guilds | len{bot.users} users', type=1)
+    activity = discord.Game(name='with {bot.users} furs', type=1)
     await bot.change_presence(status=discord.Status.online, activity=activity)
     print('ProtoPaw has started successfully')
     print('-----------')
