bc316f6f54274f9b67a840826d873f5f9e536119
Bluewy <bluewyechache@gmail.com>
2020-09-09 09:23:38 +0200

    Updated help command

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ async def test621(ctx):
 async def help(ctx):
     embed = discord.Embed(title='Commands | `pp `, `?`', color=config.color)
     embed.add_field(name="**<a:yoshiban:753151857852940309> Moderation**", value="`ban` `unban` `kick`\n`softban` `warn` `warnings`", inline=True)
-    embed.add_field(name="**🤖 Bot Related**", value="`help` `ping` `invite` `stats` `links` `info`", inline=True)
+    embed.add_field(name="**<a:TPKProtogenDance:728615119910862898> Bot Related**", value="`help` `ping` `invite` `stats` `links` `info`", inline=True)
     embed.add_field(name="**🏗️ Utils**", value="`get_id` `avatar` `serverinfo` `random` `poll` `decide` `say` `say2`", inline=True)
     embed.add_field(name="**🤝 Social**", value="`hug` `snuggle` `boop`\n `kiss` `pat` `cuddle`\n `askproto` `lick` `blush`\n`feed` `glomp` `happy`\n`highfive` `wag`", inline=True)
     embed.add_field(name="**🔞 NSFW**", value="`e621`", inline=True)
