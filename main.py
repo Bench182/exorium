07700e28f746841bff6da604230d31f39cfa9a38
Bluewy <bluewyechache@gmail.com>
2020-09-11 13:22:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -154,7 +154,7 @@ async def links(ctx):
 async def serverinfo(ctx):
     embed = discord.Embed(color=config.color)
     embed.add_field(name="Region", value=str(ctx.guild.region), inline=True)
-    embed.add_field(name="Verification Level", value=len(ctx.guild.verification_level), inline=True)
+    embed.add_field(name="Verification Level", value=str(ctx.guild.verification_level), inline=True)
     embed.set_author(name=ctx.guild.name + " information", url="https://cdn.discordapp.com/icons/" + str(ctx.guild.id) + "/" + str(ctx.guild.icon) + ".webp?size=1024", icon_url="https://cdn.discordapp.com/icons/" + str(ctx.guild.id) + "/" + str(ctx.guild.icon) + ".webp?size=1024")
     await ctx.send(embed=embed)
     await functions.logging(ctx, "Serverinfo", bot)
