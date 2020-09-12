f81008509c3f67181109493cd01407d9821ea7f7
Bluewy <bluewyechache@gmail.com>
2020-09-12 16:42:44 +0200

    aded commands.Bot and remove default help cmd

--- main.py
+++ main.py
@@ -7,3 +7,28 @@ def get_prefix(bot, message):
     prefixes = ['p/', 'pp ']
     
     return commands.when_mentioned_or(*prefixes)(bot, message)
+
+
+
+
+bot = commands.Bot(command_prefix=get_prefix)
+bot.remove_command('help')
+
+
+
+
+@bot.event
+async def on_ready():
+    activity = discord.Game(name=f'with {len(bot.users)} furs', type=1)
+    await bot.change_presence(status=discord.Status.online, activity=activity)
+    print('ProtoPaw has started successfully')
+    print('-----------')
+    print('guilds:')
+    print(len(bot.guilds))
+    print('-----------')
+    print('users:')
+    print(len(bot.users))
+    print('-----------')
+    print('version:')
+    print(discord.__version__)
+    print('-----------')
