495b27827a28c6e853787a1da27c74e9b1896f2c
Bluewy <bluewyechache@gmail.com>
2020-09-12 16:36:49 +0200

    added get_prefix

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import discord
 from discord.ext import commands
 
 import sys, traceback
+
+def get_prefix(bot, message):
+    prefixes = ['p/', 'pp ']
+    
+    return commands.when_mentioned_or(*prefixes)(bot, message)
