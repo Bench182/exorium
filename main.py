ee745ac677a2cb4b0ab3f763af5e0269450a93e4
Bluewy <bluewyechache@gmail.com>
2020-09-14 21:34:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -183,7 +183,7 @@ async def serverinfo(ctx):
     embed.add_field(name="Region", value=str(ctx.guild.region), inline=True)
     embed.add_field(name="Verification Level", value=str(ctx.guild.verification_level), inline=True)
     if ctx.guild.features:
-        embed.add_field(name="Server Features", value=str(ctx.guild.features), inline=False)
+        embed.add_field(name="Server Features", value=str(ctx.guild.features), inline=True)
     embed.add_field(name="Afk Channel", value=f"`{str(ctx.guild.afk_channel)}`\nTimeout {str(ctx.guild.afk_timeout)}s")
     embed.set_author(name=ctx.guild.name + " information", url="https://cdn.discordapp.com/icons/" + str(ctx.guild.id) + "/" + str(ctx.guild.icon) + ".webp?size=1024", icon_url="https://cdn.discordapp.com/icons/" + str(ctx.guild.id) + "/" + str(ctx.guild.icon) + ".webp?size=1024")
     await ctx.send(embed=embed)
