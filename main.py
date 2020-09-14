2dd19c37a4c5570739630eccfd53b966bfc52076
Bluewy <bluewyechache@gmail.com>
2020-09-14 21:44:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -172,9 +172,11 @@ async def links(ctx):
     await ctx.send(embed=embed)
     await functions.logging(ctx, "links", bot)
 
-COMMUNITY = 'community server' 
-NEWS = 'news channels'
 
+list features:
+    COMMUNITY = 'community server' 
+    NEWS = 'news channels'
+    
 @bot.command(name="serverinfo", aliases=["servinfo", "sinfo"])  # shows info about the server the command was executed, in an embed. Still being worked on.
 async def serverinfo(ctx):
     embed = discord.Embed(color=config.color)
@@ -185,7 +187,7 @@ async def serverinfo(ctx):
     embed.add_field(name="Region", value=str(ctx.guild.region), inline=True)
     embed.add_field(name="Verification Level", value=str(ctx.guild.verification_level), inline=True)
     if ctx.guild.features:
-        embed.add_field(name="Server Features", value=str(ctx.guild.features), inline=True)
+        embed.add_field(name="Server Features", value=getattr(features), inline=True)
     embed.add_field(name="Afk Channel", value=f"`{str(ctx.guild.afk_channel)}`\nTimeout {str(ctx.guild.afk_timeout)}s")
     embed.set_author(name=ctx.guild.name + " information", url="https://cdn.discordapp.com/icons/" + str(ctx.guild.id) + "/" + str(ctx.guild.icon) + ".webp?size=1024", icon_url="https://cdn.discordapp.com/icons/" + str(ctx.guild.id) + "/" + str(ctx.guild.icon) + ".webp?size=1024")
     await ctx.send(embed=embed)
