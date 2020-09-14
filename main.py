80c18129413cd253b081c0ff814e080c18cb6a38
Chosen-Fate <55001472+Chosen-Fate@users.noreply.github.com>
2020-09-14 20:51:13 +0200

    Fix on_guild()

--- main.py
+++ main.py
@@ -49,13 +49,14 @@ async def on_guild_join(guild):
     embed.add_field(name="Server ID", value=guild.id, inline=True)
     embed.add_field(name="Members", value=len(guild.members), inline=True)
     channel = bot.get_channel(747808767034392626)
-    return await channel.send(embed=embed)
+    await channel.send(embed=embed)
     embed = discord.Embed(color=config.color)
     embed.add_field(name="useful information", value="**Thank you for adding exorium to your server <a:shibaheartbeat:755130116316856363>**.\nexorium is a multifunctional bot to fit your needs. <a:glowpaw:755131095032397874>\nFor commands, use the `exo help` command, for more info on commands you can use `exo info {command`.")
-    embed.add_field(name="useful links", value="It's possible that you may find bugs, or errors in ProtoPaw <a:Toothlessuhmwhat:753170277915164672> Or you just want to suggest something, for that you can make an [issue](https://github.com/ThePawKingdom/exorium/issues/new/choose) in our [github repo](https://github.com/ThePawKingdom/exorium).\nFurthermore, if you just need support with exorium, you can join our [support server](https://discord.gg/RQMaHzE)!") 
-    embed.set_author(name=ctx.guild.name, url="https://cdn.discordapp.com/icons/" + str(ctx.guild.id) + "/" + str(ctx.guild.icon) + ".webp?size=1024", icon_url="https://cdn.discordapp.com/icons/" + str(ctx.guild.id) + "/" + str(ctx.guild.icon) + ".webp?size=1024")
-    embed.set_footer(text=f"exorium is now in (len{bot.guilds}) guilds with (len{bot.users} users")                          
-    await ctx.text_channels[0].send(embed=embed)
+    embed.add_field(name="useful links", value="It's possible that you may find bugs, or errors in ProtoPaw <a:Toothlessuhmwhat:753170277915164672> Or you just want to suggest something, for that you can make an [issue](https://github.com/ThePawKingdom/exorium/issues/new/choose) in our [github repo](https://github.com/ThePawKingdom/exorium).\nFurthermore, if you just need support with exorium, you can join our [support server](https://discord.gg/RQMaHzE)!")
+    embed.set_author(name=guild.name, url="https://cdn.discordapp.com/icons/" + str(guild.id) + "/" + str(guild.icon) + ".webp?size=1024", icon_url="https://cdn.discordapp.com/icons/" + str(guild.id) + "/" + str(guild.icon) + ".webp?size=1024")
+    embed.set_footer(text=f"exorium is now in (len{bot.guilds}) guilds with (len{bot.users} users")
+    return await guild.channels[0].send(embed=embed)
+
 
 @bot.command(name="ping", aliases=["pong", "latency"], brief="shows the bot's latency.")  # the ping command, simply shows the latency in an embed
 async def latency(ctx):
