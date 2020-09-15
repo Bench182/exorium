87f3914f403292bdc69fca7d3cbb520625c151f5
Bluewy <bluewyechache@gmail.com>
2020-09-15 13:00:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -581,7 +581,7 @@ async def warnings(ctx, member: discord.Member):
 @commands.is_owner()
 async def exoinfo(ctx):
     e = discord.Embed(title='exorium private information', color=config.color)
-    e.add_field(name="Guilds list", value=list(ctx.guild.guilds), inline=True)
+    e.add_field(name="Guilds list", value=len(bot.guilds), inline=True)
     await ctx.send(embed=embed)
     
         
