0b6aee05af480e98b05739e60e01c5cce76be69c
Bluewy <bluewyechache@gmail.com>
2020-09-22 11:26:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -382,7 +382,8 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.NotOwner):
         await ctx.send('Only bot owners can use this command.')  # Shows when a user executes a bot owner only command while not being a bot owner
     if isinstance(error, commands.BadArgument):
-        await ctx.send('Could not find member. Please @mention someone.') 
+        if len(ctx.args) == 1:
+            await ctx.send('Could not find member. Please @mention someone.') 
 
 @bot.command(name="askexo", aliases=["askexorium"])  # Lets you ask something to exorium, he will answer with a random answer listed in gifs.py
 async def askexorium(ctx, *, arg):
