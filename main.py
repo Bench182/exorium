a27aec85d5963e9937ffc9f1931232b557b00529
Chosen-Fate <55001472+Chosen-Fate@users.noreply.github.com>
2020-10-05 21:26:52 +0200

    Fix indentation... gotta love python

--- main.py
+++ main.py
@@ -62,12 +62,11 @@ async def del_update_stats(ctx):
     await bot.wait_until_ready()
     async with aiohttp.ClientSession() as session:
         async with session.post(f'https://api.discordextremelist.xyz/v2/bot/{bot.user.id}/stats',
-        headers={'Authorization': DELTOKEN, # Make sure you put your API Token Here
-        "Content-Type": 'application/json'},
-        data=json.dumps({'guildCount': len(bot.guilds)
-        })) as r:
+                                headers={'Authorization': config.DELTOKEN, "Content-Type": 'application/json'},
+                                data=json.dumps({'guildCount': len(bot.guilds)})
+                                ) as r:
             js = await r.json()
-            if js['error'] == True:
+            if js['error'] is True:
                 print(f'Failed to post to discordextremelist.xyz\n{js}')
 
 
